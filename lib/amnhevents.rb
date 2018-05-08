51d2b74480110850e3fbf798f231e77602cfbd06
github username <github email address>
2018-05-07 22:56:56 -0400

    Added more options to the self.list method

--- lib/amnhevents.rb
+++ lib/amnhevents.rb
@@ -1,5 +1,5 @@
 class AmnhEventsCliApp::Events
-attr_accessor :name, :type, :date, :description
+attr_accessor :name, :type, :date, :short_description
 
 def self.list
   puts "Here are the types of upcoming events at the American Museum of Natural History (AMNH):"
@@ -13,7 +13,12 @@ def self.list
   event_1.name = "Frontiers Lecture: Planetary Origin Stories"
   event_1.type = "LECTURES AND TALKS"
   event_1.date = "Monday, May 14, 2018"
-  event_1.description = "By studying and compiling “snapshots” from nearby stars, Alycia Weinberger takes us on a journey back in time to the origins of planets."
+  event_1.short_description = "By studying and compiling “snapshots” from nearby stars, Alycia Weinberger takes us on a journey back in time to the origins of planets."
+  event_1.url = "https://www.amnh.eorg/calendar/frontiers-lecture-planetary-origin-stories"
+  event_1.time = "7:30 pm"
+  event_1.location = "Enter at 81st Street"
+  event_1.tickets = "$15 ($13.50 seniors, students); $12 Members"
+  event_1.detailed_description = "Swirling disks of dust and gas surround young stars, and these disks contain the building blocks for new planets. It would take 100 million years to see a planet fully form, but luckily there are plenty of planetary systems in development for us to observe. By studying and compiling “snapshots” from nearby stars, Alycia Weinberger takes us on a journey back in time to the origins of planets."
 
 end
 

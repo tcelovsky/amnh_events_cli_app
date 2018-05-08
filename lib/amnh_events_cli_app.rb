306d7a930d0b8b4fb5f014c2252165036821a590
github username <github email address>
2018-05-07 22:03:34 -0400

    Testing bin/amnh-events file

--- lib/amnh_events_cli_app.rb
+++ lib/amnh_events_cli_app.rb
@@ -1,6 +1,8 @@
-require "amnh_events_cli_app/version"
-require_relative './amnh_events_cli_app/cli'
+require_relative "./amnh_events_cli_app/version"
+
 
 module AmnhEventsCliApp
   # Your code goes here...
 end
+
+require './lib/cli'

984e6d377ecc89f6e54b641cb6fcfe0defde0d27
github username <github email address>
2018-05-07 22:25:29 -0400

    Added menu and goodbye methods

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,6 +3,8 @@ class AmnhEventsCliApp::CLI
   def call
     puts "Welcome!"
     list_events
+    menu
+    goodbye
   end
 
   def list_events
@@ -13,5 +15,27 @@ class AmnhEventsCliApp::CLI
     puts "4. After Hours Programs"
   end
 
+  def menu
+    input = nil
+    puts "Enter the number of the event you'd like more information on, type 'list' to see the list of events again or type 'exit':"
+    while input != "exit"
+    input = gets.strip.downcase
+    case input
+    when "1"
+      puts "More information on event 1..."
+    when "2"
+      puts "More information on event 2..."
+    when "3"
+      puts "More information on event 3..."
+    when "4"
+      puts "More information on event 4..."
+    else puts "Type 'list' to see list of events again or type 'exit':"
+    end
+    end
+  end
+
+  def goodbye
+    puts "Come back soon!"
+  end
 
 end

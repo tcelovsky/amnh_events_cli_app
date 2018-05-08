417264b877c5dc709df8a48d0d1a799d639f67e7
github username <github email address>
2018-05-07 22:14:19 -0400

    Added list_events method

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,15 @@ class AmnhEventsCliApp::CLI
 
   def call
     puts "Welcome!"
+    list_events
+  end
+
+  def list_events
+    puts "Here are the types of upcoming events at the American Museum of Natural History (AMNH):"
+    puts "1. Lectures"
+    puts "2. Members Programs"
+    puts "3. Courses and Workshops"
+    puts "4. After Hours Programs"
   end
 
 

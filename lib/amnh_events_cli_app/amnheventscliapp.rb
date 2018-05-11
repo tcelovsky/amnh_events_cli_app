f04fb60d52d00f3d6eb935a4fbd483ad728a3297
tscotce <tscotce@gmail.com>
2018-05-10 22:36:00 -0400

    added a check to make sure input is within range of available events (equals number of available events)

--- lib/amnh_events_cli_app/amnheventscliapp.rb
+++ lib/amnh_events_cli_app/amnheventscliapp.rb
@@ -2,6 +2,9 @@ class AmnhEventsCliApp::Events
 attr_accessor :event, :name, :type, :date, :short_description, :url, :time, :location, :tickets, :detailed_description
 @@all = []
 
+def self.all
+  @@all
+end
 
 def self.list
   self.make_events.each.with_index(1) do |event, i|
@@ -23,19 +26,7 @@ def self.make_events
     event.short_description = @doc.css(".mod").first.css("p").text
     event.date = @doc.css(".mod").first.css("p.date").text
   end
-
-  # @@all << event_1 = self.new
-  # event_1.name = "Frontiers Lecture: Planetary Origin Stories"
-  # event_1.type = "LECTURES AND TALKS"
-  # event_1.date = "Monday, May 14, 2018"
-  # event_1.short_description = "By studying and compiling “snapshots” from nearby stars, Alycia Weinberger takes us on a journey back in time to the origins of planets."
-  # event_1.url = "https://www.amnh.eorg/calendar/frontiers-lecture-planetary-origin-stories"
-  # event_1.time = "7:30 pm"
-  # event_1.location = "Enter at 81st Street"
-  # event_1.tickets = "$15 ($13.50 seniors, students); $12 Members"
-  # event_1.detailed_description = "Swirling disks of dust and gas surround young stars, and these disks contain the building blocks for new planets. It would take 100 million years to see a planet fully form, but luckily there are plenty of planetary systems in development for us to observe. By studying and compiling “snapshots” from nearby stars, Alycia Weinberger takes us on a journey back in time to the origins of planets."
-
-  @@all
+  self.all
   # binding.pry
 end
 
--- lib/amnh_events_cli_app/cli.rb
+++ lib/amnh_events_cli_app/cli.rb
@@ -23,7 +23,7 @@ attr_accessor :events, :type
     puts "Enter the number corresponding to the type of event you'd like more information on or type 'exit':"
     while input != "exit"
       input = gets.strip.downcase
-      if input.to_i > 0
+      if input.to_i.between?(1, AmnhEventsCliApp::Events.all.length)
         event = @events[input.to_i - 1]
           puts "#{event.name}:"
           puts "#{event.date}"
